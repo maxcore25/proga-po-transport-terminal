a38ec7ef95a8614aee9c477a3d2304cd9354e0a3
maxcore25 <mkorobov2002@mail.ru>
2026-04-20 16:03:41 +0300

    Add new UI components and update dependencies
    - Introduced new components: Avatar, Card, Checkbox, Drawer, Field, Input, Label, Pagination, Popover, RadioGroup, Select, Separator, and Sheet.
    - Updated package.json to include new dependencies: @hookform/resolvers, @tanstack/react-query, @tanstack/react-table, react-hook-form, sonner, vaul, and zod.
    - Enhanced layout with TooltipProvider and Toaster components.
    - Updated pnpm-lock.yaml to reflect new dependencies and their versions.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
-
-import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner"
+import { TooltipProvider } from "@/components/ui/tooltip"
+import { cn } from "@/lib/utils"
+import { Geist_Mono, Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,10 +21,18 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        inter.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <TooltipProvider>{children}</TooltipProvider>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   )
