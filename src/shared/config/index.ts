af77702516bc8947d0e0ac2b9a40e49b1272a57c
maxcore25 <mkorobov2002@mail.ru>
2026-04-20 18:29:49 +0300

    Add roles and role type definition in configuration
    - Introduced a new constant `roles` with values 'admin' and 'user'.
    - Added a type definition `Role` to represent the possible roles based on the `roles` array.

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -11,3 +11,6 @@ export const QUERY_KEYS = {
 export const LOCAL_STORAGE_KEYS = {
   ACCESS_TOKEN: 'accessToken',
 } as const;
+
+export const roles = ['admin', 'user'] as const;
+export type Role = (typeof roles)[number];
