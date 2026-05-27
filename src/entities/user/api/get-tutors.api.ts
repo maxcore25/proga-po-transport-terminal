ed7e6b2b2888c51f0f4675233b397af544cb0458
maxcore25 <mkorobov2002@mail.ru>
2026-05-27 14:25:20 +0300

    Delete request

--- src/entities/user/api/get-tutors.api.ts
+++
@@ -1,7 +0,0 @@
-import { axiosInstance } from '@/shared/api';
-import { usersSchema } from '../model/user.schema';
-
-export const getTutors = async () => {
-  const { data } = await axiosInstance.get('/users?role=tutor');
-  return usersSchema.parse(data);
-};
